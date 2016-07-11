2b937f37aaeb32b83ed986a9f1d29ff6525d0408
Георгий Кажуро <georgij@Mac-Pro-Georgij.local>
2016-07-11 14:57:42 +0300

    added files for app

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var uncoolButton: UIButton!
+    @IBOutlet weak var coolCake: UIImageView!
+    @IBOutlet weak var coolCoffe: UIImageView!
+    @IBOutlet weak var coolCakeButton: UIButton!
+    @IBOutlet weak var coolCoffeButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBg.hidden = false
+        coolCake.hidden = false
+        coolCoffe.hidden = false
+        coolCakeButton.hidden = false
+        coolCoffeButton.hidden = false
+        uncoolButton.hidden = true
+    }
+    
+    @IBAction func uncoolCakeAction(sender: AnyObject) {
+        if coolCake.hidden == false {
+            coolCake.hidden = true
+        } else {
+            coolCake.hidden = false
+        }
+    }
+    
+    @IBAction func uncoolCoffeAction(sender: AnyObject) {
+        if coolCoffe.hidden == false {
+            coolCoffe.hidden = true
+        } else {
+            coolCoffe.hidden = false
+        }
+    }
 
 }
 
